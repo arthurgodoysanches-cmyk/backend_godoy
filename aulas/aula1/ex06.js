98b8209990880b9113f0939a68249cf9c95c3f78
arthurgodoysanches-cmyk <arthur.godoy.sanches@escola.pr.gov.br>
2026-06-11 11:49:28 +0000

    up

---
+++ aulas/aula1/ex06.js
@@ -0,0 +1,9 @@
+const numeroSecreto = 7;
+const palpite = 5;
+
+if (palpite === numeroSecreto) {
+    console.log("Parabéns! Você acertou!!");
+}
+else{
+    console.log(`Você errou!! O número era ${numeroSecreto}`);
+}
