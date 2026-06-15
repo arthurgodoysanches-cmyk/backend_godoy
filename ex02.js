b40f3190a4ffb87cdf98d82ff6bd5e3fa6fad567
arthurgodoysanches-cmyk <arthur.godoy.sanches@escola.pr.gov.br>
2026-06-15 14:53:43 +0000

    up

---
+++ ex02.js
@@ -0,0 +1,6 @@
+let base = 5;
+let altura = 2;
+let area = area * altura;
+let perimetro = 2 *(base+altura);
+
+console.log(`A aréa do retângulo: ${area}\nPerimetro do triângulo: ${perimetro}`);
---
+++ ex03.js
@@ -0,0 +1,5 @@
+let numero = 6;
+
+let tipo = n % 2 === 0 ? "par" : "impar";
+
+console.log(`o numero é ${n} é ${tipo}.`);
